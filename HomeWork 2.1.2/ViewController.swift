24bf83a139b85ecdca0d72d3e509a676ca464c23
Селбаев Алмас <almasselbayev@gmail.com>
2022-03-06 21:55:12 +0600

    Finished HomeWork 2.1.2

--- HomeWork 2.1.2/ViewController.swift
+++ HomeWork 2.1.2/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight{
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var curentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        print("Razmer: \(redLight.frame.height)")
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        print("Razmer Subviews \(redLight.frame.width)")
+    }
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START"{
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch curentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            curentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            curentLight = .green
+        default:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            curentLight = .red
+        }
+    }
+    
 }
 
